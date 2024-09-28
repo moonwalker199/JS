57b105714f3d1a36ebae6981b6adc5a89f4e20e4
Debangan Sarkar <debangan700@gmail.com>
2024-09-28 21:39:47 +0530

    day2

--- 01_var.js
+++ 01_var.js
@@ -20,3 +20,26 @@ because of issue in block scope and functional scope
 
 console.table([accountId, accountEmail, accountPassword, accountCity])
 
+//datatypes
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+//object
+
+let name="abc"
+let tenp=null
+let temp2=undefined
+let temp3= 80
+
+console.log(typeof temp2)
+
+//typeof null=object typeof undefined=undefined
+
+
+
+
+
---
+++ 02_conversion.js
@@ -0,0 +1,49 @@
+// let score=33
+// console.log(typeof score)
+
+// let valueInScore = Number(score)  //conversion, use N not n
+// console.log(typeof valueInScore)
+  //---> typeof valueInScore  will be number
+
+/* if score=33abc
+then console.log(valueInscore) will print NaN but its type will be *number*
+
+if score = null then print will be 0 and type will be number
+if score = undefined then print will be NaN and type=number
+if score=true print=1 type=number
+if score="string" print =Nan type=number
+*/
+
+let isLoggedIn = 1
+let booleanIsoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsoggedIn)    //output=true
+console.log(typeof booleanIsoggedIn)   //output=boolean
+//empty string is false while "string" is true in boolean
+
+
+//same for string to number
+
+
+// let value = 3
+// let negValue = -value   
+
+
+// let str1 = "s1"
+// let str2 = " s2"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+//**********OPERATIONS********//
+
+//basic math operations
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+//prefix and postfix operator in js
